63fd6b89e5cf38fa64e8e7dca0ed50287430c139
Raphael Pelletier-Beaulieu <stunsoirousahang@gmail.com>
2016-02-15 08:19:09 -0500

    Lecture Fichier

--- src/Application.java
+++ src/Application.java
@@ -1,8 +1,20 @@
 
 public class Application {
 
-	public static void main(String[] args) {
+
+	
+	public static void main(String[] args) throws Exception {
 		// TODO Auto-generated method stub
+	
+		//args[0] Ficher a tester : Ficher d'entree
+		WekaIO weka = new WekaIO();
+		weka.Read(args[0]);
+		System.out.println("lu");
+		
+		//args[1] Solution la plus performante : Ficher de sortie
+		
+		
+		//args[2] Solution la moins performante : Ficher de sortie
 
 	}
 
--- src/WekaIO.java
+++ src/WekaIO.java
@@ -1,5 +1,17 @@
-import weka.core.Instances;
+ import weka.core.Instances;
+import weka.core.converters.ConverterUtils.DataSource;
+
 
 public class WekaIO {
 
+	public Instances data;
+	public void Read(String path) throws Exception
+	{
+		 DataSource source = new DataSource(path);
+		 this.data = source.getDataSet();
+		 // setting class attribute if the data format does not provide this information
+		 // For example, the XRFF format saves the class attribute information as well
+		 if (this.data.classIndex() == -1)
+		   this.data.setClassIndex(this.data.numAttributes() - 1);
+	}
 }
